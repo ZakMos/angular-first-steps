004a565eaf374519622299624d51aff8c4f33aa2
ZakMos <z.zza@hotmail.com>
2019-07-02 22:22:59 +0200

    add some component about and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
