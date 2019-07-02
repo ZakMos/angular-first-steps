e5f055fb20fcac609c529916c3e441cbc89eab1a
ZakMos <z.zza@hotmail.com>
2019-07-02 22:37:14 +0200

    Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular First Steps';
+  name='Zakaria Moslim';
+  age = 43;
 }
