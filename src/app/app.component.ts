c68a917c2ec0f7a010a55c0793eeead725af65f1
ZakMos <z.zza@hotmail.com>
2019-07-02 23:29:20 +0200

    Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  colors=['Red', 'White', 'Yellow', 'Green', 'Black'];
+
   name='Zakaria Moslim';
   age = 43;
   source = 'https://picsum.photos/200/300';
+
+  myFunction() {
+      // console.log("click event");
+      alert('Hello World');
+  }
+
+  onChange() {
+    console.log("Colored Changed", this.colors);
+  }
 }
