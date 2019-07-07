a322542ba0bfd824b4a43042c27cc1eb3415e96d
ZakMos <z.zza@hotmail.com>
2019-07-08 01:00:48 +0200

    Reactive Forms

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MyserviceService } from '../myservice.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-article',
@@ -8,10 +9,19 @@ import { MyserviceService } from '../myservice.service';
 })
 export class ArticleComponent implements OnInit {
 
+  myForm=new FormGroup({
+    name:new FormControl(''),
+    age:new FormControl(''),
+    address:new FormControl(''),
+    job:new FormControl('')
+  });
+
   constructor(private service:MyserviceService) { }
 
   ngOnInit() {
     this.service.sayHello("Article");
   }
-
+  onSubmit(){
+    console.log(this.myForm.value);
+  }
 }
