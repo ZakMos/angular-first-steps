7fa632c98bb82adb545ce8eb9968413d46c490f3
ZakMos <z.zza@hotmail.com>
2019-07-08 00:22:03 +0200

    Template-driven Forms

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MyserviceService } from '../myservice.service';
+import { category } from '../category';
+
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
@@ -7,10 +9,15 @@ import { MyserviceService } from '../myservice.service';
 })
 export class CategoryComponent implements OnInit {
 
+  cat:category= new category(0, "UnKnown");
+
   constructor(private service:MyserviceService) { }
 
   ngOnInit() {
       this.service.sayHello("Category");
+  }
+  onSubmit() {
+    console.log(this.cat);
   }
 
 }
