95ffd36fa2150988cff70a6e8248cedfcb6f78cf
ZakMos <z.zza@hotmail.com>
2019-07-07 14:19:35 +0200

    add Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,50 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ArticleComponent } from './article/article.component';
+import { AboutComponent } from './about/about.component';
+import { CategoryComponent } from './category/category.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo:'',
+    pathMatch:'full'
+  },
+  {
+    path:'article',
+    component:ArticleComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'about',
+    component:AboutComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'category',
+    component:CategoryComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'login',
+    component:LoginComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    component:PageNotFoundComponent
+  },
+];
+
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+// import { RouterModule } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule} from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+import { ArticleComponent } from './article/article.component';
+import { CategoryComponent } from './category/category.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    LoginComponent
+    LoginComponent,
+    ArticleComponent,
+    CategoryComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
